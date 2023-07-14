06d165a63426fea2636aafe22173ea6d68bff4d7
MartinFortuna <martinperricault@gmail.com>
2023-07-14 18:31:47 +0000

    Add loginuser.html

--- mrwrestaurant/views.py
+++ mrwrestaurant/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
 
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+
+def auth(user):
+    if user is None or user.id is None:
+        return redirect('login')
+
 
 def index(request):
     return render(request, 'restaurant/index.html')
