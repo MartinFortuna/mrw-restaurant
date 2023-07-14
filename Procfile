8e88e43ffca61ef0fe13809eb9062b5cd59d5879
MartinFortuna <martinperricault@gmail.com>
2023-07-14 13:46:14 +0000

    Initial deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mrw.wsgi
---
+++ mrwrestaurant/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MrwrestaurantConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mrwrestaurant'
