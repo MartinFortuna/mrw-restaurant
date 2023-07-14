a5bfe16fd9adbaf6c5601b13adfb90fa95070f03
MartinFortuna <martinperricault@gmail.com>
2023-07-14 18:50:38 +0000

    Add model

--- mrwrestaurant/models.py
+++ mrwrestaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class tbBooking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    guests = models.SmallIntegerField()
+    status = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
